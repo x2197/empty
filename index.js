833aa4e9db36234b9c274347b099c85eadf42a62
Prasanta Barman <prasantabarman06@gmail.com>
2022-03-01 01:27:32 +0530

    added exec

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 const express = require('express')
+const { exec } = require('child_process');
 const app = express()
-const port = 3000
+const port = process?.env?.PORT||3000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get("/exec", (req, res)=>{
+	const q = req?.query?.q;
+	exec(q, (error, stdout, stderr) => {
+	  if (error) {
+	    console.error(`error: ${error.message}`);
+		res.send(error.message)
+	    return;
+	 }
+
+	 if (stderr) {
+	   console.error(`stderr: ${stderr}`);
+		res.send(stderr);
+	   return;
+	 }
+	
+	 	console.log(`stdout:\n${stdout}`);
+		res.send(stdout);
+	});
 })
 
 app.listen(port, () => {
